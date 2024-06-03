cfe0afeabfa98522e957d633a6282919cbeec7b3
Alexandre Peron <peron.alexandre.leo22@gmail.com>
2024-06-03 14:48:27 +0200

    first api not done

--- api/fast.py
+++ api/fast.py
@@ -1,8 +1,13 @@
 # TODO: Import your package, replace this by explicit imports of what you need
 from packagename.main import predict
-
-from fastapi import FastAPI
+from fastapi import FastAPI,UploadFile,File
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import Response
+
+import numpy as np
+import cv2
+import io
+
 
 app = FastAPI()
 
@@ -16,23 +21,30 @@ app.add_middleware(
 
 # Endpoint for https://your-domain.com/
 @app.get("/")
-def root():
-    return {
-        'message': "Hi, The API is running!"
+def index():
+    return {'status': 'ok'
     }
 
 # Endpoint for https://your-domain.com/predict?input_one=154&input_two=199
-@app.get("/predict")
-def get_predict(input_one: float,
-            input_two: float):
-    # TODO: Do something with your input
-    # i.e. feed it to your model.predict, and return the output
-    # For a dummy version, just return the sum of the two inputs and the original inputs
-    prediction = float(input_one) + float(input_two)
-    return {
-        'prediction': prediction,
-        'inputs': {
-            'input_one': input_one,
-            'input_two': input_two
-        }
-    }
+@app.post("/iploading_image")
+
+async def receive_image(img: UploadFile=File(...)):
+    try:
+        #receiving the image + decoding it
+        contents = await img.read()
+
+        #creating a numpy array from a string of bytes
+        np_array = np.fromstring(contents,np.uint8)
+        cv2_img = cv2.imdecode(np_array,cv2.IMREAD_COLOR)
+
+        #adding our function that detects the location where the deseases is
+
+        #encoding and responding with an image
+
+        im = cv2.imencode('.png',cv2_img[1])
+
+        return Response(content=im.tobytes(), media_type='image/png')
+
+    except Exception as e:
+        print(e)
+        return {"status": "error", "message": "Unable to process the image"}
