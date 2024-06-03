735117480e4e47e9109241e6945632dc177cc318
Alexandre Peron <peron.alexandre.leo22@gmail.com>
2024-06-03 17:54:29 +0200

    encoding step1 day1

--- api/fast.py
+++ api/fast.py
@@ -1,13 +1,10 @@
-# TODO: Import your package, replace this by explicit imports of what you need
-from packagename.main import predict
 from fastapi import FastAPI,UploadFile,File
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.responses import Response
-
+from starlette.responses import Response, FileResponse
+import json
 import numpy as np
 import cv2
 import io
-
 
 app = FastAPI()
 
@@ -21,30 +18,44 @@ app.add_middleware(
 
 # Endpoint for https://your-domain.com/
 @app.get("/")
-def index():
-    return {'status': 'ok'
-    }
+async def index():
+    return {"status": "ok"}
 
-# Endpoint for https://your-domain.com/predict?input_one=154&input_two=199
-@app.post("/iploading_image")
+#endoint
 
+@app.post('/predictions')
 async def receive_image(img: UploadFile=File(...)):
-    try:
-        #receiving the image + decoding it
-        contents = await img.read()
+    contents = await img.read()
+    np_array = np.fromstring(contents,np.uint8)
+    cv2_img = cv2.imdecode(np_array,cv2.IMREAD_COLOR)
 
-        #creating a numpy array from a string of bytes
-        np_array = np.fromstring(contents,np.uint8)
-        cv2_img = cv2.imdecode(np_array,cv2.IMREAD_COLOR)
+    #prediction
+    predicted_label = 'desease'
+    return {'message': predicted_label}
 
-        #adding our function that detects the location where the deseases is
 
-        #encoding and responding with an image
 
-        im = cv2.imencode('.png',cv2_img[1])
 
-        return Response(content=im.tobytes(), media_type='image/png')
+# # Endpoint for https://your-domain.com/predict?input_one=154&input_two=199
+# @app.post("/image_boxes")
 
-    except Exception as e:
-        print(e)
-        return {"status": "error", "message": "Unable to process the image"}
+# async def receive_image(img: UploadFile=File(...)):
+#     # try:
+#         #receiving the image + decoding it
+#         contents = await img.read()
+
+#         #creating a numpy array from a string of bytes
+#         np_array = np.fromstring(contents,np.uint8)
+#         cv2_img = cv2.imdecode(np_array,cv2.IMREAD_COLOR)
+
+#         #adding our function that detects the location where the deseases is
+
+#         #encoding and responding with an image
+
+#         im = cv2.imencode('.png',cv2_img)[1]
+
+#         return Response(content=im.tobytes(), media_type='image/png')
+
+    # # except Exception as e:
+    #     print(e)
+    #     return {"status": "error", "message": "Unable to process the image"}
