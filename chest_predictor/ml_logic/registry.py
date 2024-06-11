83b78b1f4f8d90a2bfb5548aff505ed602243fd6
RikVanMoll <167035377+RikFernando@users.noreply.github.com>
2024-06-11 12:03:43 +0200

    modified so that it saves in .keras instead of .h5

--- chest_predictor/ml_logic/registry.py
+++ chest_predictor/ml_logic/registry.py
@@ -43,21 +43,21 @@ def save_results(params: dict, metrics: dict) -> None:
 
 def save_model(model: keras.Model = None) -> None:
     """
-    Persist trained model locally on the hard drive at f"{LOCAL_REGISTRY_PATH}/models/{timestamp}.h5"
-    - if MODEL_TARGET='gcs', also persist it in your bucket on GCS at "models/{timestamp}.h5" --> unit 02 only
+    Persist trained model locally on the hard drive at f"{LOCAL_REGISTRY_PATH}/models/{timestamp}.keras"
+    - if MODEL_TARGET='gcs', also persist it in your bucket on GCS at "models/{timestamp}.keras" --> unit 02 only
     - if MODEL_TARGET='mlflow', also persist it on MLflow instead of GCS (for unit 0703 only) --> unit 03 only
     """
 
     timestamp = time.strftime("%Y%m%d-%H%M%S")
 
     # Save model locally
-    model_path = os.path.join(LOCAL_REGISTRY_PATH, "models", f"{timestamp}.h5")
+    model_path = os.path.join(LOCAL_REGISTRY_PATH, "models", f"{timestamp}.keras")
     model.save(model_path)
 
     print("✅ Model saved locally")
 
     if MODEL_TARGET == "gcs":
-        model_filename = model_path.split("/")[-1] # e.g. "20230208-161047.h5" for instance
+        model_filename = model_path.split("/")[-1] # e.g. "20230208-161047.keras" for instance
         client = storage.Client()
         bucket = client.bucket(BUCKET_NAME)
         blob = bucket.blob(f"models/{model_filename}")
