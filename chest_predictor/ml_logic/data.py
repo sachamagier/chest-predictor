ca958f4a531cc8e024877a543340d76bb2bf8359
Alexandre Peron <peron.alexandre.leo22@gmail.com>
2024-06-11 21:56:43 +0200

    data.py done

--- chest_predictor/ml_logic/data.py
+++ chest_predictor/ml_logic/data.py
@@ -29,12 +29,13 @@ def downloading_data(DATA_DIR,DATA_FNAME,DATA_URL):
     if Path(os.path.join(DATA_DIR, "__MACOSX")).is_dir():
 #         # remove the __MACOSX folder if it exists
         shutil.rmtree(os.path.join(DATA_DIR, "__MACOSX"))
+    print(f"✅ Data saved in your computer")
 
 #Get all image paths
     data_root = Path(data_root)
     all_image_paths = [str(path) for path in (data_root/"images"/"set_full").iterdir()]
-    # print("Get all image paths:")
-    # print(all_image_paths)
+    print("Get all image paths:")
+    print(len(all_image_paths))
 
     return all_image_paths
 
@@ -57,40 +58,41 @@ def data_clean(DATA_DIR):
                 images_to_remove.append(filename)  # add the filename to the list if it's to be removed
 
     # Print the names of the images to be removed
-    print("Images to be removed: ", images_to_remove)
+    # print("Images to be removed: ", images_to_remove)
 
-
-
-
-## uncomment the following line if you're sure about the images to be removed
-
-    # # Remove the images from the directory
-    # for filename in images_to_remove:
-    #     file_path = os.path.join(DATA_DIR, filename)
-    #     os.remove(file_path)
 
     return images_to_remove
 
 
 
-def load_and_encode_labels(DATA_PATH):
-    """
-    Load encoded labels from a CSV file. and then transform them into a tensor
+# def load_and_encode_labels(DATA_PATH_LABEL):
+#     """
+#     Load encoded labels from a CSV file. and then transform them into a tensor
 
-    Args:
-    - data_path (str): Path to the CSV file containing encoded labels
-    """
-    # Read the CSV file
-    df_labels = pd.read_csv(DATA_PATH)
+#     Args:
+#     - data_path (str): Path to the CSV file containing encoded labels
+#     """
+#     # Read the CSV file
+#     df_labels = pd.read_csv(DATA_PATH_LABEL)
 
-    # Convert the labels into a TensorFlow tensor
-    encoded_values = tf.convert_to_tensor(df_labels.values, dtype=tf.float32)
-    label_ds = tf.data.Dataset.from_tensor_slices(encoded_values)
-    # Depending on if we are doing this in another .py or not we can add the nextg line. It creates a Dataset with image paths and encoded labels
-    #path_label_ds = tf.data.Dataset.from_tensor_slices((all_image_paths, encoded_values)) --> add all_image_paths to the function parameters
-    return label_ds
+#     # Convert the labels into a TensorFlow tensor
+#     encoded_values = tf.convert_to_tensor(df_labels.values, dtype=tf.float32)
+#     label_ds = tf.data.Dataset.from_tensor_slices(encoded_values)
+#     # Depending on if we are doing this in another .py or not we can add the nextg line. It creates a Dataset with image paths and encoded labels
+#     #path_label_ds = tf.data.Dataset.from_tensor_slices((all_image_paths, encoded_values)) --> add all_image_paths to the function parameters
+#     return label_ds
 
 
 
 if __name__ == '__main__':
     downloading_data(DATA_DIR,DATA_FNAME,DATA_URL)
+    images_to_remove = data_clean(os.path.join(DATA_DIR, "resized_dataset", "images", "set_full"))
+    ## uncomment the following line if you're sure about the images to be remove
+    # # Remove the images from the directory
+    for filename in images_to_remove:
+        file_path = os.path.join(os.path.join(DATA_DIR, "resized_dataset", "images", "set_full"), filename)
+        os.remove(file_path)
+    print(f"✅ Data removed in your computer")
+
+    # Load and encode the labels
+    # label_ds = load_and_encode_labels(DATA_PATH_LABEL)
